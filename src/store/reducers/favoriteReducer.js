0c1ce479823e5f96594a220fbb80099d55c41757
svetk0 <sv.solomatnikova@gmail.com>
2024-02-21 20:11:46 +0300

    start favorite page

---
+++ src/store/reducers/favoriteReducer.js
@@ -0,0 +1,25 @@
+import { ADD_PERSON_TO_FAVORITE, REMOVE_PERSON_FROM_FAVORITE } from '@store/constants/actionTypes';
+
+const initialState = 'hello';
+
+const favoriteReducer = (state = initialState, action) => { 
+    switch (action.type) { 
+        case ADD_PERSON_TO_FAVORITE:
+            return {
+                ...state,
+                ...action.payload
+
+            }
+            case REMOVE_PERSON_FROM_FAVORITE:
+                return {
+                    ...state,
+                    ...action.payload
+    
+                }
+            
+        default:
+            return state;
+    }
+}
+
+export default favoriteReducer;
